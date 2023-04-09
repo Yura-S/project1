4bfbc28baf467408af8a27b00622ed6fed02f95d
Yura Sahakyan <yurisah@rambler.ru>
2023-04-09 18:30:29 +0000

    configured utils/main.sh

--- utils/main.sh
+++ utils/main.sh
@@ -1,1 +1,18 @@
 #!/bin/bash
+
+source ../scripts/all
+
+#----------------------------------no argument
+#CreateVpc
+
+#----------------------------------argument is subnetid
+#CreateWebServer
+
+#----------------------------------no argument
+#CreateVpcAndWebServer
+
+#----------------------------------no argument
+#CreateVpcAndWebServerAndPrivateServer
+
+#----------------------------------argument is vpc id
+#DeleteVpc
